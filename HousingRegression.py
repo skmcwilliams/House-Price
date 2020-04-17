c39ad502f2a663e5dad45ef5ea34619b1243b4ac
SKM <skm@Stevens-MacBook-Air.local>
2020-04-17 14:51:18 -0500

    code & readme update

--- HousingRegression.py
+++ HousingRegression.py
@@ -4,11 +4,12 @@ import pandas as  pd
 import numpy as np
 import scipy
 from matplotlib import pyplot as plt
-from sklearn.linear_model import LinearRegression
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.cluster import KMeans
 from yellowbrick.cluster import KElbowVisualizer
 from sklearn import metrics
-from sklearn.metrics import mean_squared_log_error
+from sklearn.metrics import mean_squared_log_error, accuracy_score
 from math import sqrt
 from sklearn.model_selection import cross_val_predict, cross_val_score
 from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
@@ -20,16 +21,20 @@ import seaborn as sbn
 train = pd.read_csv("train.csv", index_col=0)
 test = pd.read_csv("test.csv", index_col=0)
 
+"""
 # Check shape of test data
 print(train.shape)
 print(test.shape)
+print(train.head())"""
 
 
 
 # Drop Square footage over 4,000, as that is a large outlier
-train.drop(train[train.GrLivArea > 4000].index, inplace=True)
+train.drop(train[train.GrLivArea > 4000].index, inplace = True)
+train.drop(train[train.SalePrice > 450000].index, inplace = True)
 
 # Clean data by interpolating nulls, sum of nulls is now 0
+
 cleantrain = train.select_dtypes(include=[np.number]).interpolate()
 
 
@@ -42,13 +47,24 @@ print(sum(cleantrain.isnull().sum() != 0))
 """clean test data same as train data"""
 
 # Clean data by interpolating nulls, sum of nulls is now 0
+test.drop(train[train.GrLivArea > 4000].index, inplace = True)
+test.drop(train[train.SalePrice > 450000].index, inplace = True)
 cleantest = test.select_dtypes(include=[np.number]).interpolate()
-# Did not drop based on square footage for full data consumption
 
 # CleanTest Dataframe shown below - copy to console
 cleantest.shape
 cleantest
 print(sum(cleantest.isnull().sum() != 0))
+
+"""Distribution of Sale Prices, skewed right due to large amount of homes
+    selling between $100k and $200k"""
+plt.figure(figsize = (10,7))
+plt.tight_layout()
+sbn.distplot(cleantrain[['SalePrice']])
+plt.title('SalePrice Distribution')
+print('Skewed Distribution: ', scipy.stats.skew(cleantrain[['SalePrice']]))
+print('Raw Data Points:/n', cleantrain.SalePrice.describe())
+plt.show()
 
 
 """
@@ -85,15 +101,17 @@ class Variables:
         poly_features = PolynomialFeatures(degree=degrees, include_bias=False)
         X_poly = poly_features.fit_transform(x)
         model = LinearRegression()
-        model.fit(X_poly,self.y)
+        model.fit(X_poly, self.y)
+        y_pred = abs(model.predict(X_poly))
+        
 
         # Plot
         plt.figure(figsize=(10,7))
-        X_plot_poly = poly_features.fit_transform(x)
-        y_pred = abs(model.predict(X_plot_poly))
+        #X_plot_poly = poly_features.fit_transform(x)
+        #y_pred = abs(model.predict(X_poly))
         plt.scatter(x, self.y, c ='b', edgecolors = (0, 0, 0))
         r2 = metrics.r2_score(self.y, y_pred)
-        plt.plot(X_plot_poly, y_pred,'-r', label = 'R-Square: ' + str(r2))
+        plt.plot(X_poly, y_pred,'-r', label = 'R-Square: ' + str(r2))
         if len(np.unique([self.x])) > 11:
             plt.xlabel('Gross Living Area (sf)')
             plt.title('Gross Living Area vs. Sale Price ' + str(degrees) + ' Degree')
@@ -150,14 +168,6 @@ def MLR():
 
     MLR_Y = cleantrain['SalePrice'].values
     
-    """Distribution of Sale Prices, skewed right due to large amount of homes
-    selling between $100k and $200k"""
-    plt.figure(figsize = (10,7))
-    plt.tight_layout()
-    sbn.distplot(MLR_Y)
-    plt.show()
-    print('Skewed Distribution: ', scipy.stats.skew(MLR_Y))
-    
     # Build predictions and plot
     regressor = LinearRegression()
     regressor.fit(MLR_X, MLR_Y) 
@@ -179,15 +189,14 @@ def MLR():
     print('Mean Absolute Error:', metrics.mean_absolute_error(MLR_Y, y_pred))  
     print('Mean Squared Error:', metrics.mean_squared_error(MLR_Y, y_pred))  
     print('Root Mean Squared Log Error:', np.sqrt(metrics.mean_squared_log_error(MLR_Y, y_pred)))
-
-
-    # Run KFolds for MLR
+    
+    # Run KFolds
     scores = cross_val_score(regressor, MLR_X, MLR_Y, cv=10)
-    print('KFold Scores for MLR Training Model:' + str(scores))
+    print('KFold Scores for MLogReg Training Model:' + str(scores))
     mean = round(scores.mean()*100,2)
     print('Average Accuracy: ' + str(mean) + '%')
     
-
+    
 def Test_MLR():
     """Selected MLR, run on Test Data"""
     # Select Data for MLR 
@@ -196,7 +205,55 @@ def Test_MLR():
     X_Test = cleantest[[variable for variable in cleantest if variable != 'SalePrice']].values
     
     # Run predictions based on test data
-    regressor = LinearRegression()
+    regressor = LinearRegression(normalize = True, n_jobs = 1000)
+    regressor.fit(MLR_X, MLR_Y)
+    y_test_pred = abs(regressor.predict(X_Test))
+    return y_test_pred
+
+
+def log():
+    """Because none of the models were accurate to be reach my goal of
+    top 25% in the competition, leet's see if MLR is better"""
+    # Create variables for MLR model
+    MLR_X = preprocessing.scale(cleantrain[[variable for variable in cleantrain
+                                            if variable != 'SalePrice']].values)
+
+    MLR_Y = cleantrain['SalePrice'].values
+    
+    # Build predictions and plot
+    regressor = LogisticRegression(penalty = 'l2', max_iter = 5000, n_jobs = 3)
+    regressor.fit(MLR_X, MLR_Y) 
+    y_pred = abs(regressor.predict(MLR_X))
+    plt.figure(figsize = (10,7))
+    plt.scatter(MLR_Y, y_pred, edgecolors = (0, 0, 0)) 
+    plt.title("Sale Prices")
+    plt.xlabel("Actual Sale Prices")
+    plt.ylabel('Predicted Sale Price')
+    plt.show()
+    time.sleep(1)
+    # Visualize Actuals vs. Predicted
+    preds = pd.DataFrame({'Actual': MLR_Y, 'Predicted': y_pred})
+    print(preds.head(25))
+
+    scores = accuracy_score(MLR_Y, y_pred)
+    print('\nRESULTS OF TRAIN MLogReg MODEL:')
+    print('Mean Absolute Error:', metrics.mean_absolute_error(MLR_Y, y_pred))  
+    print('Mean Squared Error:', metrics.mean_squared_error(MLR_Y, y_pred))  
+    print('Root Mean Squared Log Error:', np.sqrt(metrics.mean_squared_log_error(MLR_Y, y_pred)))
+    print('Score for MLogReg Training Model:' + str(scores))
+    
+
+def Test_MLogR():
+    """Selected MLR, run on Test Data"""
+    # Select Data for MLR 
+    MLR_X = preprocessing.scale(cleantrain[[variable for variable 
+    in cleantrain if variable != 'SalePrice']].values)
+    MLR_Y = cleantrain['SalePrice'].values
+    X_Test = preprocessing.scale(cleantest[[variable for variable in cleantest
+                                            if variable != 'SalePrice']].values)
+    
+    # Run predictions based on test data
+    regressor = LogisticRegression(penalty = 'l2', max_iter = 5000, n_jobs = 3)
     regressor.fit(MLR_X, MLR_Y)
     y_test_pred = abs(regressor.predict(X_Test))
     return y_test_pred
@@ -230,26 +287,27 @@ def Sale_KMeans(k):
 quality = Variables(variables[0])
 livingarea = Variables(variables[1])
 
-# Call Regression, Degrees 2 for Quality performs best. No accurate enough for Kaggle
-quality.regressionplot(2)
-livingarea.regressionplot(1)
+"""Below code blocked out, used LogReg for Kaggle submission"""
+#quality.regressionplot(2)
+#livingarea.regressionplot(1)
 
 # Plot KMeans Charts
-quality.KMeans(4)
-livingarea.KMeans(4)
-Sale_KMeans(4)
+#quality.KMeans(4)
+#Sale_KMeans(4)
 
 
 # Run MLR to see how this performs **best result in RMSLE and KFolds***
-MLR()
-
+#MLR()
+#log()
 
 """Call Test_MLR as predictions to create list of predicted housing prices to be
 pushed to dataframe and csv file"""
-predictions = Test_MLR()
+predictions = Test_MLogR()
 
-"""#submit to kaggle
+"""
+#submit to kaggle
 submit = pd.DataFrame()
+#submit['id'] = test['id']
 submit['SalePrice'] = predictions
 print(submit.head(20))
 submit.to_csv('sample_submission.csv', index = False)
--- README.txt
+++ README.txt
@@ -4,68 +4,59 @@
 This program reads through a training dataset and allows the user to identify
 the best course of action for predicting housing prices of the test dataset.
 
-Lines 3-16: Required Packages to complete analysis
-
-Lines 19-51: Clean train dataset. Resulted in some increased accuracy, apply same methods to test dataset
-            note that the test dataset did not remove the houses > 4000 sf, as the full data should be read in the test
+Clean train dataset. Resulted in some increased accuracy, apply same methods to test dataset
     
 The program then runs through my through process and decision in predicitng the house prices.
 
-Lines 60-65: Produce a list of data points that have a 70% correlation or greater, returned only two results
-            Note that the results only have a 58% correlation with each other, this shows that there are more variables contribute
-            in some meaningful way, out of the 79 variables total
+Produce a list of data points that have a 70% correlation or greater, returned only two results
+Note that the results only have a 58% correlation with each other, this shows that there are more variables contribute
+in some meaningful way, out of the 79 variables total
     
     
-Lines 75-142: The Variables class runs different models using the two most
-            valuable variables, Overall Quality and Gross Living Area. This
-            module runs regression model and then calculates KFolds, as well as
-            scipy.stats and accuracy scores
-            (quality = Variables(variables[0]) - line 230
-            livingarea = Variables(variables[1])) - line 231
+The Variables class runs different models using the two most valuable variables, Overall Quality and Gross Living Area. This
+module runs regression model and then calculates KFolds, as well as scipy.stats and accuracy scores
+(quality = Variables(variables[0]) - line 230
+livingarea = Variables(variables[1])) - line 231
 
             
-    Regression: This forms a regression model for both variables, predicting
-                the Sales Price. The degree does not impact Gross Living Area,
-                as the increase in r-squared was minimal. After moving to Degree 2,
-                the r-square of Overall Quality did not change enough to warrant
-                chasing r-squared with degree increases. As such, i stuck with
-                Degree 2 for Gross Living Area and Degree 1 for Overall Quality
-                Called on lines 234 and 235
+Regression: This forms a regression model for both variables, predicting
+        the Sales Price. The degree does not impact Gross Living Area,
+        as the increase in r-squared was minimal. After moving to Degree 2,
+        the r-square of Overall Quality did not change enough to warrant
+        chasing r-squared with degree increases. As such, i stuck with
+        Degree 2 for Gross Living Area and Degree 1 for Overall Quality
+        
                 
-    KMeans: This produces an elbow method chart for the model so the user knows
-            which KMeans to select. This was more of a learning exercise than 
-            anything, as the data from the KMeans showed a clear divide  at
-            Overall Quality level 6 and split the Gross Living Area data in 
-            quadrants near the center. This would have been more valuable if we
-            really needed to dig in and find the cause behind the clusters, but
-            it won't necessarily help us predict Housing Prices. Instatiated
-            on lines 238 and 239
+KMeans: This produces an elbow method chart for the model so the user knows
+        which KMeans to select. This was more of a learning exercise than 
+        anything, as the data from the KMeans showed a clear divide  at
+        Overall Quality level 6 and split the Gross Living Area data in 
+        quadrants near the center. This would have been more valuable if we
+        really needed to dig in and find the cause behind the clusters, but
+        it won't necessarily help us predict Housing Prices.
                             
                             
                             
-Lines 145-202:  These lines run MLR, which produced better results than the previous
-                models.
-                
-    MLR:        Runs MLR with 36 variables compared to the Y (Sale Price). Produces
-                accuracy results, foremost RMSLE, which is what what Kaggle uses to
-                prove accuracy. Called on line 244
+MLR:    Runs MLR with 36 variables compared to the Y (Sale Price). Produces
+        accuracy results, foremost RMSLE, which is what what Kaggle uses to
+        prove accuracy. 
             
-    Test_MLR:   Runs MLR with the test dataset, returns to the calling variable
-                to be exported to the submission CSV file. Called on line 249
+Test_MLR:   Runs MLR with the test dataset, returns to the calling variable
+        to be exported to the submission CSV file. Called on line 249
                 
-Lines 205-225:  Run KMeans for Sale Price
+LogisticRegression: Added to the thought process, results in more accurate restults, but not reflected on Kaggle, as 
+                        submission rank only improved by ~200 entrants
                 
 NOTES:
 If the user wants to change the correlation used to identify variables, change
-0.70 to your desired correlation score on lines 70 and 71
+0.70 to your desired correlation score
 
 If the user wants to change degrees used in the Linear Regression model, 
-change the integer in the call variable (234 and 235) to the desired degree level
+change the integer in the call variable to the desired degree level
 
-Can change K in KMeans by changing integer on lines 238-240
+Can change K in KMeans by changing integer where called
 
-Submission to Kaggle has taken place via codes 251-256, code blocked to avoid 
-unnecessary updating of submission file
+Submission to Kaggle code blocked to avoid unnecessary updating of submission file
 
 
             
